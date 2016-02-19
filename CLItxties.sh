eea4d0929afc2d9e7c594b273db5e1d2f82e041a
parazyd <parazyd@dyne.org>
2016-02-19 02:21:05 +0100

    minor edits, personal preferences

--- CLItxties.sh
+++ CLItxties.sh
@@ -1,16 +1,14 @@
-#!/bin/env bash
-
+#!/bin/bash
 # quickly post something to http://txti.es
-
-# Required: curl, lynx, nano
+# Required: curl, lynx, $EDITOR
 
 # create temporary file
-TempFile="cl_txti"`date +%s-%N`
+TempFile=`date +%s-%N`"-cl_txti.md"
 touch /tmp/$TempFile
 
 # get input either interactivly or via arguments
 if [ "$1" = "-i" ]; then
-	nano /tmp/$TempFile
+	$EDITOR /tmp/$TempFile
 	content=$(</tmp/$TempFile)
 	rm /tmp/$TempFile
 	echo -n "Custom URL: "
@@ -24,7 +22,11 @@ elif [ "$1" = "-f" ]; then
 fi
 
 # try to post and check if URL already exists
-curl -s --data-urlencode "content=$content" --data "&custom_url=$custom_url&custom_edit_code=$custom_edit_code&form_level=2&submit=Save%20and%20done" http://txti.es | lynx -nostatus -stdin -dump -nolist |  grep -q "already exists" 
+curl -s --data-urlencode "content=$content" \
+    --data "&custom_url=$custom_url&custom_edit_code=$custom_edit_code&form_level=2&submit=Save%20and%20done" \
+        http://txti.es \
+    | lynx -nostatus -stdin -dump -nolist \
+    | grep -q "already exists" 
 
 # if URL does already exist, get a new one interactivly or produce random one
 if [ "$1" = "-i" ]; then
@@ -32,7 +34,12 @@ if [ "$1" = "-i" ]; then
 	do
 	   	echo -n "This url (\"$custom_url\") is already in use, please choose a new one: "      
 	   	read custom_url
-	   	curl -s --data-urlencode "content=$content" --data "&custom_url=$custom_url&custom_edit_code=$custom_edit_code&form_level=2&submit=Save%20and%20done" http://txti.es | lynx -nostatus -stdin -dump -nolist | grep -q "already exists" 
+	   	curl -s --data-urlencode "content=$content" --data \
+            "&custom_url=$custom_url&custom_edit_code=$custom_edit_code&form_level=2&submit=Save%20and%20done" \
+                http://txti.es \
+            | lynx -nostatus -stdin -dump -nolist \
+            | grep -q "already exists" 
+
 	  	if [ $? -eq 1 ]; then
 	  		echo "Your post is online at: http://txti.es/$custom_url"
 			echo "Your edit code is: $custom_edit_code"
@@ -45,11 +52,25 @@ elif [ "$1" = "-f" ]; then
 	while [ $? -eq 0 ]
 	do
 	   	rnd_url=$custom_url`date +-%s`
-	   	curl -s --data-urlencode "content=$content" --data "&custom_url=$rnd_url&custom_edit_code=$custom_edit_code&form_level=2&submit=Save%20and%20done" http://txti.es | lynx -nostatus -stdin -dump -nolist | grep -q "already exists" 
+	   	curl -s --data-urlencode "content=$content" --data \
+                "&custom_url=$rnd_url&custom_edit_code=$custom_edit_code&form_level=2&submit=Save%20and%20done" \
+                http://txti.es \
+            | lynx -nostatus -stdin -dump -nolist \
+            | grep -q "already exists" 
+
 	  	if [ $? -eq 1 ]; then
 	  		echo "http://txti.es/$rnd_url $custom_edit_code"
 			break
 		fi
 	done
-fi
+elif [ "$*" = "" ]; then
+    cat <<EOF
+Usage:
+    CLItxties.sh -i (enter content and custom url/editcode interactively)
+    CLItxties.sh -f [path-to-file] [custom url] [custom edit code] (post contents of file)
 
+If you used MyCoolPost as customized URL you'll find your post at
+http://txti.es/MyCoolPost and the edit link would be http://txti.es/MyCoolPost/edit
+EOF
+
+fi
